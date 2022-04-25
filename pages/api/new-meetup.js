e09289a41950625a1e5ef3e451f8cad5f423f0b0
Shane Rafferty <shaner9@hotmail.co.uk>
2022-04-25 13:29:23 +0100

    MongoDB added

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,25 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://shaner:X1FFY8qVQ5yYi3AE@cluster0.vxyoc.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result); //try catch to error handle if you wish
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
-  return test;
 }
 
 export default handler;
